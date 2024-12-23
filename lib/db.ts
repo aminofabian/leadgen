4aa0b93a2912134307aa5bfb2cdee6a9ac373050
Fabian Amino <aminofab@gmail.com>
2024-12-23 20:25:09 +0300

    addded

--- lib/db.ts
+++ lib/db.ts
@@ -1,41 +1,15 @@
-import { PrismaClient } from '@prisma/client'
-import { Pool, PoolConfig } from '@neondatabase/serverless'
-import { PrismaNeon } from '@prisma/adapter-neon'
-import ws from 'ws'
-
-declare global {
-  var prisma: ReturnType<typeof prismaClientSingleton> | undefined
-}
-
-interface CustomPoolConfig extends PoolConfig {
-  webSocketConstructor: any;
-}
-
-const connectionString = process.env.DIRECT_URL!
-const pool = new Pool({ 
-  connectionString, 
-  max: 1, 
-  webSocketConstructor: ws
-} as CustomPoolConfig)
-const adapter = new PrismaNeon(pool)
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient().$extends({
-    query: {
-      $allModels: {
-        async $allOperations({ operation, model, args, query }) {
-          return query(args);
-        },
-      },
-    },
-  });
-}
+  return new PrismaClient();
+};
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma
-}
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export const db = prisma
-export default prisma
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
