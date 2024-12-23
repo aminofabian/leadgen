69fddb71096b8aabd3d2362e55e39803ebf95154
Fabian Amino <aminofab@gmail.com>
2024-12-23 20:19:52 +0300

    addded

--- lib/db.ts
+++ lib/db.ts
@@ -1,22 +1,33 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import ws from 'ws'
 
 declare global {
-  var prisma: PrismaClient | undefined
+  var prisma: ReturnType<typeof prismaClientSingleton> | undefined
 }
 
+const connectionString = process.env.DIRECT_URL!
+const pool = new Pool({ connectionString, max: 1, webSocketConstructor: ws as unknown as any } as const)
+const adapter = new PrismaNeon(pool)
+
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log: ['error', 'warn'],
-    errorFormat: 'minimal'
-  })
+  return new PrismaClient().$extends({
+    query: {
+      $allModels: {
+        async $allOperations({ operation, model, args, query }) {
+          return query(args);
+        },
+      },
+    },
+  });
 }
 
 const prisma = globalThis.prisma ?? prismaClientSingleton()
-
-export const db = prisma
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = prisma
 }
 
+export const db = prisma
 export default prisma
